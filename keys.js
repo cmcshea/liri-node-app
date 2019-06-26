d9ebd7000239edf504144cbc5c5a2c47a570a4db
Your Name <cory.mcshea@gmail.com>
2019-06-26 15:48:16 -0400

    imputting spotify api info

--- keys.js
+++ keys.js
@@ -3,6 +3,6 @@ var keys = require("./keys.js");
 console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: process.env.f17a5d1059c84f779f433d26124cb9c5,
+    secret: process.env.ff6b06d7a71d43609e01b8110631b858
 };
