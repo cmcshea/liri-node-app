2685860a240eef1ba6d3db87ffb2284c39aa7bf7
cmcshea <cory.mcshea@gmail.com>
2019-06-30 21:04:57 -0400

    updated command lines

--- liri.js
+++ liri.js
@@ -1,19 +1,24 @@
 require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
+// var axios = require("axios");
+// var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2]
 var searchTerm = process.argv.splice(3).join(" ")
 
-console.log(command)
+console.log(searchTerm)
 
 if (command === "spotify-this-song") {
     SpotifySong(searchTerm);
-} else {
+} else if (command === "concert-this") {
+    concertThis(searchTerm);
+ } else if (command === "movie-this") {
+    console.log(searchTerm)
+ } else 
     console.log("Invalid command")
-}
 
 function SpotifySong(song){
     spotify.search({ type: 'track', query: song }, function (err, data) {
